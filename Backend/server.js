065526f72ff10333751a0d34559b8db7910fc70e
“Sangini <sanginijshetty@gmail.com>
2025-01-03 11:31:31 +0530

    Second commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import connectDB from './config/db';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
 
 //configure env
 dotenv.config()
 
+//database confige
+connectDB();
+
 //rest object
 const app = express()
 
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth', authRoutes)
 //rest api
 app.get('/', (req,res) => {
     res.send({
-        message: 'Welcome to Skin Serenity'
+        message: '<h1>Welcome to Skin Serenity</h1>'
     })
 })
 
