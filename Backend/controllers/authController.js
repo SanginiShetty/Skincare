cec45b984280e1c0ed9c439f963f4d969f270f07
“Sangini <sanginijshetty@gmail.com>
2025-01-04 18:36:41 +0530

    4 commit

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,1 +1,43 @@
-export const registerController = () => {};
+import userModel from "../models/userModel";
+import {hashedPassword} from './../helpers/authHelper';
+
+export const registerController = async(req, res) => {
+    try {
+        const {name, email, password, phone, address}= req.body
+        //validations
+        if(!name) {
+            return res.send({error: 'Name is Required'})
+        }
+        if(!email) {
+            return res.send({error: 'Email is Required'})
+        }
+        if(!password) {
+            return res.send({error: 'Password is Required'})
+        }
+        if(!phone) {
+            return res.send({error: 'Phone number is Required'})
+        }
+        if(!address) {
+            return res.send({error: 'Address is Required'})
+        }
+        //check user
+        const existingUser = await userModel.findOne({email})
+
+        //existing user
+        if(existingUser){
+            return res.status(200).send({
+                success: true,
+                message:'Alreaady Registered'
+            })
+        }
+        //register user
+        const hashedPassword = await hashedPassword()
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message: 'Error in Registration',
+            error
+        })
+    }
+};
