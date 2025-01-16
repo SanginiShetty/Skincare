cdde50f4fbcd1aa9f747a36bd286418b49fb381b
“Sangini <sanginijshetty@gmail.com>
2025-01-16 17:56:40 +0530

    9th commit

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -14,8 +14,19 @@ export const requireSignIn = async (req, res, next) => {
 };
 
 //admin access
-export const isAdmin = (req,res,next) => {
-    try{}catch(error){
+export const isAdmin = async(req,res,next) => {
+    try{
+        const user =  await userModel.findById(req.user)
+        if(user.role !== 1){
+            return res.status(401).send({
+                success:false,
+                message:'UnAuthorized Access',
+            });
+        }
+        else {
+            next();
+        }
+    }catch(error){
         console.log(error)
     }
 }
