8166c1f2102eb0b890f44c0303ce6b0f317ed47e
“Sangini <sanginijshetty@gmail.com>
2025-01-17 18:18:21 +0530

    11th commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Layout from "./components/layout/Layout.js"
 
 function App() {
   return (
     <>
+    <Layout>
      <h1>SkinCare App</h1>
+    </Layout>
     </>
   );
 }
