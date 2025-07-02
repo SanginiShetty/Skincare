9e6b342b12cd1f1bd558be79a17b89c0f0cdaa67
“Sangini <sanginijshetty@gmail.com>
2025-07-02 18:05:20 +0530

    register form

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,30 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../../components/layout/Layout";
 
 const Register = () => {
+  const {name, setName} = useState("")
+  const {email, setEmail} = useState("")
+  const {password, setPassword} = useState("")
+  const {phone, setPhone} = useState("")
+  const {address, setAddress} = useState("")
+
   return (
     <Layout title="Register-Skincare App">
       <div className="register">
         <h1>Register Page</h1>
         <form>
           <div className="mb-3">
-            <label htmlFor="exampleInputName" className="form-label">
-              Name
-            </label>
             <input
               type="text"
+              value={name}
+              onChange = {(e)=> setName(e.target.value)}
               className="form-control"
               id="exampleInputEmail1"
+              placeholder="Enter your name"
+              required
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-3">
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="form-control"
               id="exampleInputPassword1"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your phone number"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your address"
+              required
             />
           </div>
           <button type="submit" className="btn btn-primary">
