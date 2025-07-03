30e8193eabf74fa5e260e4a28e5bf4a0c00c338e
“Sangini <sanginijshetty@gmail.com>
2025-07-03 21:19:32 +0530

    .

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,40 +4,12 @@ import axios from "axios";
 import {useNavigate} from 'react-router-dom';
 import toast from 'react-hot-toast';
 import "../../styles/AuthStyles.css";
-const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const navigate = useNavigate()
-
-  //form function
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(
-        `${process.env.REACT_APP_API}/api/v1/auth/register`,
-        { name, email, password, phone, address }
-      );
-      if(res.data.success){
-        toast.success(res.data.message)
-        navigate('/login')
-      }
-      else {
-        toast.error(res.data.message)
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong");
-    }
-  };
-
+const Login = () => {
   return (
-    <Layout title="Register-Skincare App">
+    <Layout title="Login-Skincare App">
       <div className="form-container ">
         <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
+          <h4 className="title">LOGIN</h4>
           <div className="mb-3">
             <input
               type="text"
@@ -94,7 +66,7 @@ const Register = () => {
             />
           </div>
           <button type="submit" className="btn btn-primary">
-            REGISTER
+            LOGIN
           </button>
         </form>
       </div>
@@ -102,4 +74,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
